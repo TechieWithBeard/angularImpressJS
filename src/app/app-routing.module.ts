01a5c529fc53132e765b4136ca32c5be74e71f40
vishnu Thankappan <vishnu.thankappan@maistering.com>
2021-06-01 09:30:58 +0530

    impressjs integrated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Slide3dComponent } from './slide3d/slide3d.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: '', component: IntroComponent },
+  { path: '3d', component: Slide3dComponent, data: { history: null } },
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Slide3dComponent } from './slide3d/slide3d.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Slide3dComponent
   ],
   imports: [
     BrowserModule,
